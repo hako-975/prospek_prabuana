7b24b3092c584a7fcc7d39e9026ebb42718ba32c
Andri Firman Saputra <andrifirmansaputra1@gmail.com>
2023-02-15 12:21:29 +0700

    tambah konsumen

---
+++ footer.php
@@ -0,0 +1,16 @@
+<!-- JavaScript Libraries -->
+<script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.bundle.min.js"></script>
+<script src="lib/chart/chart.min.js"></script>
+<script src="lib/easing/easing.min.js"></script>
+<script src="lib/waypoints/waypoints.min.js"></script>
+<script src="lib/owlcarousel/owl.carousel.min.js"></script>
+<script src="lib/tempusdominus/js/moment.min.js"></script>
+<script src="lib/tempusdominus/js/moment-timezone.min.js"></script>
+<script src="lib/tempusdominus/js/tempusdominus-bootstrap-4.min.js"></script>
+<script src="lib/datatables/jquery.dataTables.min.js"></script>
+<script src="lib/datatables/dataTables.bootstrap5.min.js"></script>
+<script src="lib/sweetalert2/sweetalert2.all.min.js"></script>
+
+<!-- Template Javascript -->
+<script src="js/main.js"></script>
---
+++ navbar.php
@@ -0,0 +1,22 @@
+<nav class="navbar navbar-expand bg-light navbar-light sticky-top px-4 py-0">
+    <a href="index.php" class="navbar-brand d-flex d-lg-none me-4">
+    </a>
+    <a href="#" class="sidebar-toggler flex-shrink-0">
+        <i class="fa fa-bars"></i>
+    </a>
+    <div class="d-none d-md-flex ms-4">
+        <a href="" class="btn btn-primary"><i class="fas fa-fw fa-plus"></i> Tambah Prospek</a>
+    </div>
+    <div class="navbar-nav align-items-center ms-auto">
+        <div class="nav-item dropdown">
+            <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown">
+                <img class="rounded-circle me-lg-2" src="img/user.jpg" alt="" style="width: 40px; height: 40px;">
+                <span class="d-none d-lg-inline-flex">Akhsya Ubaidika</span>
+            </a>
+            <div class="dropdown-menu dropdown-menu-end bg-light border-0 rounded-0 rounded-bottom m-0">
+                <a href="#" class="dropdown-item">My Profile</a>
+                <a href="#" class="dropdown-item">Log Out</a>
+            </div>
+        </div>
+    </div>
+</nav>
